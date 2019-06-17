d3218dab55f6954c9f3ca7ecd2af4c703689b71b
HindrikStegenga <Hindrik1997@hotmail.com>
2019-06-17 18:50:00 +0200

    Added VC.
    Signed-off-by: HindrikStegenga <Hindrik1997@hotmail.com>

--- Gamedesign game/ViewControllers and Views/TutorialViewController.swift
+++ Gamedesign game/ViewControllers and Views/TutorialViewController.swift
@@ -11,6 +11,27 @@ import Cocoa
 
 class TutorialViewController : NSViewController {
     
+    @IBOutlet var textView: NSTextView!
+    @IBOutlet weak var continueButton: NSButton!
+    
+    let textSets: [String] = [
+        """
+        Ow no... the dreaded day has come! The day of your retirement!
+        Sadly, your hair is becoming gray, but today is also your lucky day.
+        You won a coupon to do free shopping at the grocery store.
+        You must collect as many anti-gray-hair shampoo bottles as you can in one minute.
+        3... 2... 1...
+        """
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textView.string = textSets.first!
+    }
     
     
+    @IBAction func didPressContinueBtn(_ sender: NSButton) {
+        self.view.window?.performClose(nil)
+    }
 }
